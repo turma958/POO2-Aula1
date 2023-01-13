4f22f68656c7620bbf008a90423cd22c858cb32d
Davi Nascimento <davign20@outlook.com>
2023-01-13 17:22:56 -0300

    first commit

---
+++ Aula1/Program.cs
@@ -0,0 +1,92 @@
+﻿var leao = new Leao
+{
+    Nome = "Lion",
+    Idade = 16
+};
+
+var leaoFilho = new Leao
+{
+    Nome = "Lion filho",
+    Idade = 1
+};
+
+Amamentar(leao, leaoFilho);
+Console.ReadLine();
+
+var reino = new Rinoceronte
+{
+    Nome = "Dino",
+    Idade = 22
+};
+
+void Amamentar(Mamifero mae, Mamifero filho)
+{
+    mae.Amamentar();
+    filho.Mamar();
+}
+
+abstract class Animalia
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+}
+
+abstract class Mamifero : Animalia
+{
+    public void Mamar()
+    {
+        Console.WriteLine("Mamando");
+    }
+
+    public void Amamentar()
+    {
+        Console.WriteLine("Amamentando");
+    }
+
+    public void Comer()
+    {
+        Console.WriteLine($"Eu como: {ObterTipoAlimento()}");
+    }
+
+    public abstract string ObterTipoAlimento();
+}
+
+abstract class Carnivoro : Mamifero
+{
+}
+
+abstract class Herbivoro : Mamifero
+{
+}
+
+class Leao : Carnivoro
+{
+    public override string ObterTipoAlimento()
+    {
+        return "antilopes!";
+    }
+}
+
+class Jacare : Carnivoro
+{
+    public override string ObterTipoAlimento()
+    {
+        return "peixes!";
+    }
+}
+
+class Rinoceronte : Herbivoro
+{
+    public override string ObterTipoAlimento()
+    {
+        return "plantas!";
+    }
+}
+
+class Coelho : Herbivoro
+{
+    public override void Comer()
+    {
+        Console.WriteLine("Eu como grama!");
+    }
+}
---
+++ Aula1/Program2.cs
@@ -0,0 +1,49 @@
+﻿//var aluno = new Aluno();
+//aluno.Nome = "Davi";
+//aluno.Email = "davi@teste.com";
+
+//var professor = new Professor();
+//professor.Nome = "Davi prof";
+//professor.Email = "davi@teste.com";
+
+////aluno.Salvar();
+//Printar(aluno);
+//Printar(professor);
+
+//Console.ReadLine();
+
+//void Printar(Pessoa pessoa)
+//{
+//    //Console.WriteLine($"{pessoa.Nome} - {pessoa.Email}");
+//    pessoa.ExibirInformacoes();
+//}
+
+//abstract class Pessoa
+//{
+//    // Atributos
+//    public string Nome { get; set; }
+//    public string CPF { get; set; }
+//    public string Email { get; set; }
+//    public string Senha { get; set; }
+
+//    // Ações
+//    public abstract void ExibirInformacoes();
+//}
+
+//class Aluno : Pessoa
+//{
+//    public int Nota { get; set; }
+
+//    public override void ExibirInformacoes()
+//    {
+//        Console.WriteLine($"{Nome} - {Nota}");
+//    }
+//}
+
+//class Professor : Pessoa
+//{
+//    public override void ExibirInformacoes()
+//    {
+//        Console.WriteLine($"{Nome} - {Email}");
+//    }
+//}
